4e5c2649cd4f0d1d6de88086e5231b4743a181cb
Harsh-Mohta9163 <mohta.harsh9163@gmail.com>
2024-04-07 19:37:03 +0530

    deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4ju2n@$f9d0c=h)_g0lbb%k9&@rf(xa$d$g$&5ri$uf)*gev^4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 AUTH_USER_MODEL = 'authentication.CustomUser'
 CSRF_TRUSTED_ORIGINS = ["https://*.replit.dev", "https://*.replit.app"]
 
